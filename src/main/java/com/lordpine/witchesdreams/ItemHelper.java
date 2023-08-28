a30802dc735d4b373b61d4ac858c2aa4dd9baecf
LordPINE <middelhylke@gmail.com>
2023-08-28 19:49:48 +0200

    Clean up formatting with spotless

--- src/main/java/com/lordpine/witchesdreams/ItemHelper.java
+++ src/main/java/com/lordpine/witchesdreams/ItemHelper.java
@@ -19,35 +19,35 @@ public class ItemHelper {
 
     public static boolean matchesStack(ItemStack stack, String stackString) {
         stackString = stackString.replace("\"", "");
-            String[] splitString = stackString.split(":");
-            if (splitString.length < 2 || splitString.length > 3) {
-                WitchesDreams.LOG.warn(stackString + " is not a valid item ID, ignoring!");
-                return false;
-            }
-            // ore dictionary handling
-            if (splitString[0] == "ore" && OreDictionary.doesOreNameExist(splitString[1])) {
-                for (int ID : OreDictionary.getOreIDs(stack)) {
-                    if (OreDictionary.getOreID(splitString[1]) == ID) {
-                        return true;
-                    }
-                }
-            } else {
-                Item item = GameRegistry.findItem(splitString[0], splitString[1]);
-                if (stack.getItem()
-                    .equals(item)) {
-                    if (splitString.length == 3) {
-                        if (splitString[2] == "*") return true;
-                        try {
-                            if (Integer.parseInt(splitString[2]) == stack.getItemDamage()) {
-                                return true;
-                            }
-                        } catch (Exception ignored) {}
-                    } else if (stack.getItemDamage() == 0) {
-                        return true;
-                    }
-                }
-            }
+        String[] splitString = stackString.split(":");
+        if (splitString.length < 2 || splitString.length > 3) {
+            WitchesDreams.LOG.warn(stackString + " is not a valid item ID, ignoring!");
             return false;
+        }
+        // ore dictionary handling
+        if (splitString[0] == "ore" && OreDictionary.doesOreNameExist(splitString[1])) {
+            for (int ID : OreDictionary.getOreIDs(stack)) {
+                if (OreDictionary.getOreID(splitString[1]) == ID) {
+                    return true;
+                }
+            }
+        } else {
+            Item item = GameRegistry.findItem(splitString[0], splitString[1]);
+            if (stack.getItem()
+                .equals(item)) {
+                if (splitString.length == 3) {
+                    if (splitString[2] == "*") return true;
+                    try {
+                        if (Integer.parseInt(splitString[2]) == stack.getItemDamage()) {
+                            return true;
+                        }
+                    } catch (Exception ignored) {}
+                } else if (stack.getItemDamage() == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public static ItemStack createStackFromString(String stackString) {
